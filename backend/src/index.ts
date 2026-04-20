1d72be9d15d616d531107560b02e131473c9d2a9
Ryan Bernstein <ryan@ryanjbernstein.com>
2026-04-20 11:10:03 -0400

    cors for public

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 import sequelize from './config/database';
 import requireAuth from './middleware/requireAuth';
 import publicRoutes from './routes/publicRoutes'
@@ -17,6 +18,21 @@ app.use(express.json());
 
 // Routes
 
+const allowedOrigins = [
+  "https://catch23-public.vercel.app",
+  "https://get-catch23.vercel.app"
+];
+
+app.use(cors({
+    origin:( origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS origin not allowed"));
+        }
+    },
+    credentials: true
+}));
 app.use('/api/create-key', create); //make an account 
 app.use('/api/', requireAuth);
 app.use('/api/public', publicRoutes); //Think about naming this smthn different
