df210b6ea3f348ef01478afbf37ba1d47c88dcb6
Ryan Bernstein <ryan@ryanjbernstein.com>
2026-04-22 21:50:26 -0400

    fixing bcrypt logic

--- backend/src/controllers/accountController.ts
+++ backend/src/controllers/accountController.ts
@@ -12,6 +12,7 @@ export const create = async (req: Request, res: Response): Promise<void> => {
             return;
         }
         const passwordHash = await bcrypt.hash(password, 10);
+        console.log(passwordHash);
         const apiUser = await createApiUser(email, passwordHash);
         res.status(201).json(apiUser.apiKey);
     } catch (err: any) {
@@ -36,8 +37,7 @@ export const login = async(req: Request, res: Response): Promise<void> => {
         return;
     }
 
-    const passwordHash = await bcrypt.hash(password, 10);
-    const valid = await bcrypt.compare(passwordHash, user.passwordHash);
+    const valid = await bcrypt.compare(password, user.passwordHash);
     // const valid = password === user.passwordHash;
     console.log('password:', password);
     console.log('hash:', user.passwordHash);
