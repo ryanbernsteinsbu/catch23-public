1cd09541e2ac8c3ce171d88bcdcd3968cbf16f32
Ryan Bernstein <ryan@ryanjbernstein.com>
2026-04-22 21:25:18 -0400

    remove bcrypt from frontend

--- backend/src/controllers/accountController.ts
+++ backend/src/controllers/accountController.ts
@@ -11,8 +11,8 @@ export const create = async (req: Request, res: Response): Promise<void> => {
             res.status(400).json({error: "Email and password required"});
             return;
         }
-        // const passwordHash = await bcrypt.hash(password, 10);
-        const apiUser = await createApiUser(email, password);
+        const passwordHash = await bcrypt.hash(password, 10);
+        const apiUser = await createApiUser(email, passwordHash);
         res.status(201).json(apiUser.apiKey);
     } catch (err: any) {
         res.status(400).json({error: err.message});
@@ -36,7 +36,8 @@ export const login = async(req: Request, res: Response): Promise<void> => {
         return;
     }
 
-    const valid = await bcrypt.compare(password, user.passwordHash);
+    const passwordHash = await bcrypt.hash(password, 10);
+    const valid = await bcrypt.compare(passwordHash, user.passwordHash);
     // const valid = password === user.passwordHash;
     console.log('password:', password);
     console.log('hash:', user.passwordHash);
